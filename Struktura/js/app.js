07f75c51b67ebef6f6169bafc2d3fc4fcf099d12
DanCvejn <daniel.cvejn@pslib.cz>
2021-03-01 21:19:31 +0100

    login commit

---
+++ Struktura/js/app.js
@@ -0,0 +1,21 @@
+$('.reg').click(function(){
+    $(this).removeClass('darker');
+    $('.log').addClass('darker');
+    $('.register').removeClass('notactive');
+    $('.login').addClass('notactive');
+    $('.register').addClass('active');
+    $('.login').removeClass('active');
+    $('.container').removeClass('mt-15');
+    $('.container').addClass('mt-5');
+})
+
+$('.log').click(function(){
+    $(this).removeClass('darker');
+    $('.reg').addClass('darker');
+    $('.login').removeClass('notactive');
+    $('.register').addClass('notactive');
+    $('.register').removeClass('active');
+    $('.login').addClass('active');
+    $('.container').removeClass('mt-5');
+    $('.container').addClass('mt-15');
+})
