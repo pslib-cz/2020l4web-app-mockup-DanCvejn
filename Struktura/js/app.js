97a7eeede3ab2f562f56a1d987392f73e594b23c
DanCvejn <daniel.cvejn@pslib.cz>
2021-04-08 21:54:34 +0200

    lit changes commit

--- Struktura/js/app.js
+++ Struktura/js/app.js
@@ -1,24 +1,3 @@
-$('.reg').click(function(){
-    $(this).removeClass('darker');
-    $('.log').addClass('darker');
-    $('.register').removeClass('notactive');
-    $('.login').addClass('notactive');
-    $('.register').addClass('active');
-    $('.login').removeClass('active');
-    $('.container').removeClass('mt-15');
-    $('.container').addClass('mt-5');
-})
-
-$('.log').click(function(){
-    $(this).removeClass('darker');
-    $('.reg').addClass('darker');
-    $('.login').removeClass('notactive');
-    $('.register').addClass('notactive');
-    $('.register').removeClass('active');
-    $('.login').addClass('active');
-    $('.container').removeClass('mt-5');
-    $('.container').addClass('mt-15');
-})
 
 $('.menuOpener').click(function(){
     $('.menu').removeClass('close');
@@ -28,11 +7,3 @@ $('.menuCloser').click(function(){
     $('.menu').addClass('close');
 })
 
-$('.block').click(function(){
-    $('.block-off').toggleClass('block-on');
-    $('.menu').addClass('close');
-})
-
-$('.result').click(function(){
-    $('.block-off').removeClass('block-on');
-})
